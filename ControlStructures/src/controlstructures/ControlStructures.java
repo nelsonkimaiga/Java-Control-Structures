275b8f6b7411e0272b08f7af148e4c5baf79d516
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-09-15 10:12:12 +0300

    initial

---
+++ ControlStructures/src/controlstructures/ControlStructures.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlstructures;
+
+/**
+ *
+ * @author Nelson Kimaiga
+ */
+public class ControlStructures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //For Loop
+         int[] numbers = 
+             {1,2,3,4,5,6,7,8,9,10, 11, 12, 13,14, 15, 16, 17, 18, 19 ,20};
+         for (int item : numbers) {
+             System.out.println("The count is: " + item);
+         }
+    }
+}// TODO code application logic here
+    
+    
---
+++ ControlStructures/src/controlstructures/DoWhile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlstructures;
+
+/**
+ *
+ * @author Nelson Kimaiga
+ */
+public class DoWhile {
+ public static void main(String[] args) {
+    int count = 20;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count < 20);
+    }
+}
+
